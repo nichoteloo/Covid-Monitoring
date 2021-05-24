384fbc2d7f4e7ee2883440f9841f766bce3d9503
nichoteloo <nicolaschristianto@mail.ugm.ac.id>
2021-05-25 03:21:12 +0700

    modified main.py

--- main.py
+++ main.py
@@ -3,13 +3,14 @@ import pdb
 import json
 import yaml
 import urllib.request
-from flask import Flask, Markup, render_template, request
 from flask_mysqldb import MySQL
+from flask import Flask, Markup, render_template, request
 
 app = Flask(__name__)
 
 BASE_URL = "https://api.covid19api.com/"
 SUMMARY = "summary"
+COUNTRIES = "countries"
 TOTAL_FROM_DAY_ONE_BY_COUNTRY = "total/dayone/country/"
 
 # configure db
@@ -22,22 +23,36 @@ app.config['MYSQL_DB'] = db['mysql_db']
 # instanciate
 mysql = MySQL(app)
 
+def callApi(route):
+	with urllib.request.urlopen(BASE_URL + route) as url:
+		data = json.loads(url.read().decode())
+		return data
+
+def allCountry():
+	totalCountry = callApi(COUNTRIES)
+	
+	for x in totalCountry:
+		country = x["Country"]
+		slug = x["Slug"]
+		iso2 = x["ISO2"]
+		# pdb.set_trace()
+
+		cur = mysql.connection.cursor()
+		cur.execute("INSERT INTO `countries` (`Country`, `Slug`, `ISO2`) VALUES (%s,%s);", (country , slug, iso2))
+		mysql.connection.commit()
+		cur.close()
+
 def getCountry():
 	cur = mysql.connection.cursor()
-	cur.execute("SELECT Country FROM countries")
+	cur.execute("SELECT Country FROM countries;")
+	if 
 	countries = cur.fetchall()
 	cur.close()
-
 	option = ""
 	for country in countries:
 		option = option + "<option value='" + str(country[0]).lstrip().replace(" ","-") + "'>" + str(country[0]).lstrip() + "</option>\n"
 	htmlOptions = Markup(option)
 	return htmlOptions
-
-def callApi(route):
-	with urllib.request.urlopen(BASE_URL + route) as url:
-		data = json.loads(url.read().decode())
-		return data
 
 @app.route('/')
 def index():
@@ -54,7 +69,7 @@ def chart():
 		dataType = request.args.get('dataType')
 
 		totalCountry = callApi(TOTAL_FROM_DAY_ONE_BY_COUNTRY + country)
-		pdb.set_trace()
+		
 		return render_template('/index.html')
 
 if __name__ == '__main__':
