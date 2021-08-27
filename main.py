499f49b697d7b3b1e5c1bcc0e3ce279c23541890
nichoteloo <nicolaschristianto@mail.ugm.ac.id>
2021-08-27 23:51:04 +0700

    continue making routing API for cases-from-day-one

--- main.py
+++ main.py
@@ -1,76 +1,51 @@
 import os
-import pdb
-import json
-import yaml
-import urllib.request
-from flask_mysqldb import MySQL
-from flask import Flask, Markup, render_template, request
+import urls
+import requests
+from helper_ import getCountries, getSlugCountry, getOptionCountries, callApi
+from flask import Flask, render_template, request, send_from_directory
 
 app = Flask(__name__)
 
-BASE_URL = "https://api.covid19api.com/"
-SUMMARY = "summary"
-COUNTRIES = "countries"
-TOTAL_FROM_DAY_ONE_BY_COUNTRY = "total/dayone/country/"
-
-# configure db
-db = yaml.safe_load(open('db.yaml'))
-app.config['MYSQL_HOST'] = db['mysql_host']
-app.config['MYSQL_USER'] = db['mysql_user']
-app.config['MYSQL_PASSWORD'] = db['mysql_password']
-app.config['MYSQL_DB'] = db['mysql_db']
-
-# instanciate
-mysql = MySQL(app)
-
-def callApi(route):
-	with urllib.request.urlopen(BASE_URL + route) as url:
-		data = json.loads(url.read().decode())
-		return data
-
-def allCountry():
-	totalCountry = callApi(COUNTRIES)
-	
-	for x in totalCountry:
-		country = x["Country"]
-		slug = x["Slug"]
-		iso2 = x["ISO2"]
-		# pdb.set_trace()
-
-		cur = mysql.connection.cursor()
-		cur.execute("INSERT INTO `countries` (`Country`, `Slug`, `ISO2`) VALUES (%s,%s);", (country , slug, iso2))
-		mysql.connection.commit()
-		cur.close()
-
-def getCountry():
-	cur = mysql.connection.cursor()
-	cur.execute("SELECT Country FROM countries;")
-	if 
-	countries = cur.fetchall()
-	cur.close()
-	option = ""
-	for country in countries:
-		option = option + "<option value='" + str(country[0]).lstrip().replace(" ","-") + "'>" + str(country[0]).lstrip() + "</option>\n"
-	htmlOptions = Markup(option)
-	return htmlOptions
-
 @app.route('/')
 def index():
-	optionHtml = getCountry()
-	totalCases="-"
-	totalDeaths="-"
-	totalRecovered="-"
-	return render_template('/index.html', totalCases=totalCases, totalDeaths=totalDeaths, countriesOptions=optionHtml, totalRecovered=totalRecovered)
+	totalCases = "-"
+	totalDeaths = "-"
+	totalRecovered = "-"
+	
+	optionHTML = getOptionCountries()
+
+	return render_template('/index.html', totalCases=totalCases, totalDeaths=totalDeaths, totalRecovered=totalRecovered, countriesOptions=optionHTML)
 
 @app.route('/chart', methods=['GET'])
 def chart():
 	if request.method == 'GET':
 		country = request.args.get('country')
 		dataType = request.args.get('dataType')
+		if bool(country) & bool(dataType):
+			temp = []
+			optionHTML = getOptionCountries()
+			slug = getSlugCountry(country)
 
-		totalCountry = callApi(TOTAL_FROM_DAY_ONE_BY_COUNTRY + country)
-		
-		return render_template('/index.html')
+			totalCountry = callApi(urls.TOTAL_FROM_DAY_ONE_BY_COUNTRY + slug[0][0])
+
+			for i in range(len(totalCountry)):
+				if totalCountry[i]['Country'] == country:
+					temp.append(totalCountry[i])
+					break
+				else:
+					temp = []
+
+			if temp[0]['Confirmed'] is not None:
+				print(temp[0]['Confirmed'])
+				totalConfirmed = str(temp[0]['Confirmed'])
+			if temp[0]['Deaths'] is not None:
+				print(temp[0]['Deaths'])
+				totalDeaths = str(temp[0]['Deaths'])
+			if temp[0]['Recovered'] is not None:
+				print(temp[0]['Recovered'])
+				totalRecovered = str(temp[0]['Recovered'])
+
+	return render_template('/index.html', totalCases = totalConfirmed, totalDeaths = totalDeaths, totalRecovered = totalRecovered, countriesOptions=optionHTML)
 
 if __name__ == '__main__':
 	app.run(debug=True)
